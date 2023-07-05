eb8fa18b5770f0ef2d89b9b669f81ccc79245fd2
viphakorn <viphakone585@gmail.com>
2023-07-05 21:59:20 +0700

    href

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ import Logo from "@/assets/svg/logo.svg"
 export default function Footer() {
   return (
     <footer className="footer container mx-auto flex flex-col items-center gap-2 border-t border-sky-400/10 py-4 text-center">
-      <Link href={"/"} className="logo flex items-center gap-2">
+      <Link href="/" className="logo flex items-center gap-2">
         <ExportedImage src={Logo} alt="" className="w-16" />
         <h2 className="bg-gradient-to-r from-sky-400 to-sky-500 bg-clip-text text-2xl font-black text-sky-400 text-transparent">Laos TextBooks</h2>
         <span className="sr-only">ປຶ້ມແບບຮຽນ ປຶ້ມຄູ່ມືຄູ</span>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,7 +13,7 @@ export default function Header() {
     <header className="header sticky top-0 bg-white shadow shadow-sky-400/10 dark:bg-slate-900">
       <div className="container relative flex h-16 items-center justify-between">
         <NavigationMenu />
-        <Link href={"/"} className="logo flex items-center gap-2">
+        <Link href="/" className="logo flex items-center gap-2">
           <ExportedImage src={Logo} alt="" className="w-16" />
           <h2 className="bg-gradient-to-r from-sky-400 to-sky-500 bg-clip-text text-2xl font-black text-sky-400 text-transparent">Laos TextBooks</h2>
           <span className="sr-only">ປຶ້ມແບບຮຽນ ປຶ້ມຄູ່ມືຄູ</span>
